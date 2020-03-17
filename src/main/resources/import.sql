64bac6557bb23fd99444474ec329b92d816b7f2e
gamunax <gamunaxx@gmail.com>
2020-03-17 18:04:44 -0500

    changes

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+/* Populate Tables */
+
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Andres', 'Guzman', 'profesor@educati.edu', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'Jhon', 'Martinez', 'estudiante@educati.edu', '2020-01-02');
